a5b4d1af628474f92cc50ed3793d166c00091b05
Mikiyas T <tolkomikiyas46@gmail.com>
2025-12-22 11:24:44 -0800

    Removed dbt example folder

--- taxi_dbt_model/models/example/my_first_dbt_model.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
-
-with source_data as (
-    select 1 as id  -- only non-null rows
-    -- you can add more rows here, just make sure id is never null
-)
-
-select *
-from source_data
